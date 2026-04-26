01e9a23ef08a68aa8b177aa078c317996cf4d70f
Mohamed Samy <30407140105993@sci.asu.edu.eg>
2026-04-27 01:43:26 +0300

    Update User.cs

--- backend/KooraSpot/KooraSpot/Models/User.cs
+++ backend/KooraSpot/KooraSpot/Models/User.cs
@@ -11,8 +11,8 @@ namespace KooraSpot.Models
 
          // Profile information
         public string? Name { get; set; }
-       //public string? PhoneNumber { get; set; } 
-        // public string? City { get; set; }
+       public string? PhoneNumber { get; set; }
+        public string? City { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
