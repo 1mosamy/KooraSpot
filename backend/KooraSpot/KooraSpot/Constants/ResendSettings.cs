bdb5fc31803ca54d6a4bd3ed239d0023a71f9255
Mohamed Samy <30407140105993@sci.asu.edu.eg>
2026-05-11 21:00:29 +0300

    .

---
+++ backend/KooraSpot/KooraSpot/Constants/ResendSettings.cs
@@ -0,0 +1,11 @@
+﻿//namespace KooraSpot.Constants
+//{
+//    public static class ResendSettings
+//    {
+//        public const string ApiKey = "re_feuyrzQU_HocYJRF1YDAS3xKV4emP7eQN";
+
+//        public const string SenderEmail = "onboarding@resend.dev";
+
+//        public const string SenderName = "KooraSpot";
+//    }
+//}
